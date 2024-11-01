34fd8baa18c8542a49c243fe72e6fa97dfac8631
ejjordan <ejjordan12@gmail.com>
2024-11-01 21:09:50 +0100

    add tasks to find where new lambda should run

--- airflowHPC/dags/anthracene.py
+++ airflowHPC/dags/anthracene.py
@@ -25,17 +25,17 @@ dagrun_params = {
             "gro": {
                 "directory": "anthracene/gro",
                 "filename": [
-                    "lam00.gro",
-                    "lam01.gro",
-                    "lam02.gro",
-                    "lam03.gro",
-                    "lam04.gro",
-                    "lam05.gro",
-                    "lam06.gro",
-                    "lam07.gro",
-                    "lam08.gro",
-                    "lam09.gro",
-                    "lam10.gro",
+                    "lam000.gro",
+                    "lam010.gro",
+                    "lam020.gro",
+                    "lam030.gro",
+                    "lam040.gro",
+                    "lam050.gro",
+                    "lam060.gro",
+                    "lam070.gro",
+                    "lam080.gro",
+                    "lam090.gro",
+                    "lam100.gro",
                 ],
             },
             "top": {"directory": "anthracene", "filename": "anthracene.top"},
@@ -125,7 +125,7 @@ with DAG(
         .partial(mdp_json_file_path=mdp_json)
         .expand(update_dict=mdp_options)
     )
-    lambda_vals = get_lambdas_from_mdp(
+    lambda_dirs = get_lambdas_from_mdp.override(task_id="get_lambda_dirs")(
         mdp_json, "vdw_lambdas", mdp_options, "init-lambda-state", prefix="lambda_"
     )
     grompp_input_list = prepare_gmx_input_named(
@@ -141,7 +141,7 @@ with DAG(
             "{{ params.output_dir }}",
             "iteration_{{ params.iteration }}",
         ],
-        names=lambda_vals,
+        names=lambda_dirs,
     )
     grompp = ResourceGmxOperatorDataclass.partial(
         task_id="grompp",
@@ -184,7 +184,13 @@ with DAG(
 
 
 @task
-def TI(dhdl_files, output_dir, temp: float = 300.0, equil_index: int = 10):
+def TI(
+    dhdl_files,
+    output_dir,
+    temp: float = 300.0,
+    equil_index: int = 10,
+    states: list = None,
+):
     import logging, os
     from alchemlyb.parsing.gmx import extract_dHdl
     from alchemlyb.preprocessing import subsampling
@@ -220,13 +226,30 @@ def TI(dhdl_files, output_dir, temp: float = 300.0, equil_index: int = 10):
     logging.info(f"TI error: {ti.d_delta_f_}")
     logging.info(f"TI error endpoint difference: {ti.d_delta_f_.loc[0.0, 1.0]}")
 
+    logging.info(f"lambda values: {states}")
+    state_uncertainties = []
+    for i, state in enumerate(states[:-1]):
+        neighbor_uncertainty = ti.d_delta_f_.loc[float(state), float(states[i + 1])]
+        logging.info(
+            f"Uncertainty between lambda {state} and lambda {states[i + 1]}: {neighbor_uncertainty}"
+        )
+        state_uncertainties.append((state, states[i + 1], neighbor_uncertainty))
+    max_uncertainty = max(state_uncertainties, key=lambda x: x[2])
+
     # Plotting
     ax = plot_ti_dhdl(ti, labels=["VDW"], colors="r")
     ax.figure.savefig(os.path.join(output_dir, "dhdl_TI.png"))
+    return max_uncertainty
 
 
 @task
-def MBAR(dhdl_files, output_dir, temp: float = 300.0, equil_index: int = 10):
+def MBAR(
+    dhdl_files,
+    output_dir,
+    temp: float = 300.0,
+    equil_index: int = 10,
+    states: list = None,
+):
     import logging, os
     from alchemlyb.parsing.gmx import extract_u_nk
     from alchemlyb.preprocessing import subsampling
@@ -266,11 +289,135 @@ def MBAR(dhdl_files, output_dir, temp: float = 300.0, equil_index: int = 10):
     logging.info(f"MBAR error: {mbar.d_delta_f_}")
     logging.info(f"MBAR error endpoint difference: {mbar.d_delta_f_.loc[0.0, 1.0]}")
 
+    logging.info(f"lambda values: {states}")
+    state_uncertainties = []
+    for i, state in enumerate(states[:-1]):
+        neighbor_uncertainty = mbar.d_delta_f_.loc[float(state), float(states[i + 1])]
+        logging.info(
+            f"Uncertainty between lambda {state} and lambda {states[i+1]}: {neighbor_uncertainty}"
+        )
+        state_uncertainties.append((state, states[i + 1], neighbor_uncertainty))
+    max_uncertainty = max(state_uncertainties, key=lambda x: x[2])
+
     # Plotting
     ax = plot_mbar_overlap_matrix(mbar.overlap_matrix)
     ax.figure.savefig(
         os.path.join(output_dir, "O_MBAR.png"), bbox_inches="tight", pad_inches=0.0
     )
+    return max_uncertainty
+
+
+@task(multiple_outputs=True)
+def generate_lambda_states(num_states: int):
+    """
+    Generates a dictionary of lambda states for a given number of states.
+    The keys are the state index and the values are the lambda value.
+    The lambda values are uniformly spaced between 0 and 1, rounded to 2 decimal places.
+    """
+    idx_to_state = {
+        str(i): f"{round(i / (num_states - 1), 2):.2f}" for i in range(num_states)
+    }
+    state_to_idx = {v: k for k, v in idx_to_state.items()}
+    states = list(idx_to_state.values())
+    indices = list(idx_to_state.keys())
+    return {
+        "idx_to_state": idx_to_state,
+        "state_to_idx": state_to_idx,
+        "states": states,
+        "indices": indices,
+    }
+
+
+@task
+def get_new_state_ti(ti_results, lambda_states, **context):
+    import logging
+    import random
+    import ast
+
+    ti_state_idx1 = lambda_states["state_to_idx"][ti_results[0]]
+    ti_state_idx2 = lambda_states["state_to_idx"][ti_results[1]]
+    assert ti_state_idx1 < ti_state_idx2
+    new_ti_state_idx = random.randrange(int(ti_state_idx1) + 1, int(ti_state_idx2) - 1)
+
+    params = context["params"]
+    logging.info(
+        f"TI max uncertainty between states {ti_results[0]} and {ti_results[1]}: {ti_results[2]}"
+    )
+    logging.info(
+        f"TI max uncertainty state indices: {ti_state_idx1} and {ti_state_idx2}"
+    )
+    logging.info(
+        f"new ti state: {new_ti_state_idx}: {lambda_states['idx_to_state'][str(new_ti_state_idx)]}"
+    )
+
+    new_mdp_options = [
+        ast.literal_eval(opt) if type(opt) is str else opt
+        for opt in params["mdp_options"]
+    ]
+    # Add new ti state to mdp_options and sort
+    new_mdp_options.append({"init-lambda-state": new_ti_state_idx})
+    new_mdp_options = sorted(new_mdp_options, key=lambda x: x["init-lambda-state"])
+
+    # Add new ti state to gro files. Pick the gro file with the closest lambda value to the new state, without going over
+    gro_files = params["inputs"]["gro"]["filename"]
+    for i, gro in enumerate(gro_files):
+        if float(new_ti_state_idx) < float(gro[3:6]):
+            gro_to_copy = gro_files[i - 1]
+            break
+    logging.info(f"gro file to copy: {gro_to_copy}")
+    return {
+        "mdp_options": new_mdp_options,
+        "gro_to_copy": gro_to_copy,
+        "new_gro_idx": new_ti_state_idx,
+        "new_gro_fn": f"lam{new_ti_state_idx:03d}.gro",
+    }
+
+
+@task
+def get_new_state_mbar(mbar_results, lambda_states, **context):
+    import logging
+    import random
+    import ast
+
+    mbar_state_idx1 = lambda_states["state_to_idx"][mbar_results[0]]
+    mbar_state_idx2 = lambda_states["state_to_idx"][mbar_results[1]]
+    assert mbar_state_idx1 < mbar_state_idx2
+    new_mbar_state_idx = random.randrange(
+        int(mbar_state_idx1) + 1, int(mbar_state_idx2) - 1
+    )
+
+    params = context["params"]
+    logging.info(
+        f"MBAR max uncertainty between states {mbar_results[0]} and {mbar_results[1]}: {mbar_results[2]}"
+    )
+    logging.info(
+        f"MBAR max uncertainty state indices: {mbar_state_idx1} and {mbar_state_idx2}"
+    )
+    logging.info(
+        f"new mbar state: {new_mbar_state_idx}: {lambda_states['idx_to_state'][str(new_mbar_state_idx)]}"
+    )
+
+    new_mdp_options = [
+        ast.literal_eval(opt) if type(opt) is str else opt
+        for opt in params["mdp_options"]
+    ]
+    # Add new mbar state to mdp_options and sort
+    new_mdp_options.append({"init-lambda-state": new_mbar_state_idx})
+    new_mdp_options = sorted(new_mdp_options, key=lambda x: x["init-lambda-state"])
+
+    # Add new mbar state to gro files. Pick the gro file with the closest lambda value to the new state, without going over
+    gro_files = params["inputs"]["gro"]["filename"]
+    for i, gro in enumerate(gro_files):
+        if float(new_mbar_state_idx) < float(gro[3:6]):
+            gro_to_copy = gro_files[i - 1]
+            break
+    logging.info(f"gro file to copy: {gro_to_copy}")
+    return {
+        "mdp_options": new_mdp_options,
+        "gro_to_copy": gro_to_copy,
+        "new_gro_idx": new_mbar_state_idx,
+        "new_gro_fn": f"lam{new_mbar_state_idx:03d}.gro",
+    }
 
 
 with DAG(
@@ -307,14 +454,30 @@ with DAG(
         dataset_path="{{ params.output_dir }}/iteration_{{ params.iteration }}/{{ params.expected_output }}",
         key="dhdl",
     )
+    mdp_options = unpack_mdp_options()
+    mdp_json = get_file.override(task_id="get_mdp_json")(
+        input_dir="{{ params.inputs.mdp.directory }}",
+        file_name="{{ params.inputs.mdp.filename }}",
+    )
+    lambda_vals = get_lambdas_from_mdp.override(task_id="get_states")(
+        mdp_json, "vdw_lambdas", mdp_options, "init-lambda-state"
+    )
     ti = TI.override(task_id="TI")(
         dhdl_files=get_dhdl_files,
         output_dir="{{ params.output_dir }}/iteration_{{ params.iteration }}",
+        states=lambda_vals,
     )
     mbar = MBAR.override(task_id="MBAR")(
         dhdl_files=get_dhdl_files,
         output_dir="{{ params.output_dir }}/iteration_{{ params.iteration }}",
+        states=lambda_vals,
     )
+    get_states = generate_lambda_states(101)
+    ti_results = get_new_state_ti(ti_results=ti, lambda_states=get_states)
+    mbar_results = get_new_state_mbar(mbar_results=mbar, lambda_states=get_states)
+
     is_anthracene_done >> anthracene_done_branch >> [trigger_anthracene, get_dhdl_files]
     trigger_anthracene >> get_dhdl_files
     get_dhdl_files >> [ti, mbar]
+    ti >> ti_results
+    mbar >> mbar_results
