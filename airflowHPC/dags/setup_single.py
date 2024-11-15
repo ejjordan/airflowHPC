f0362cb480babf4a6189889cfa8281e73d7e401c
ejjordan <ejjordan12@gmail.com>
2024-11-15 16:22:22 +0100

    Make parameters for setup_single DAG configurable

--- airflowHPC/dags/setup_single.py
+++ airflowHPC/dags/setup_single.py
@@ -24,20 +24,62 @@ with DAG(
     catchup=False,
     render_template_as_native_obj=True,
     max_active_runs=1,
-    params={"output_dir": "setup_single"},
+    params={
+        "output_dir": "setup_single",
+        "setup": {
+            "inputs": {
+                "pdb": {
+                    "directory": "ala_pentapeptide",
+                    "filename": "ala_pentapeptide.pdb",
+                }
+            },
+            "box_size": 3.2,
+            "force_field": "amber99sb-ildn",
+            "water_model": "tip3p",
+            "ion_concentration": 0.15,
+            "expected_output": "system_prepared",
+            "output_dir": "prep",
+        },
+        "minimize": {
+            "inputs": {
+                "mdp": {"directory": "mdp", "filename": "min.json"},
+            },
+            "expected_output": "em",
+            "output_dir": "em",
+        },
+        "nvt": {
+            "inputs": {
+                "mdp": {"directory": "mdp", "filename": "nvt.json"},
+            },
+            "expected_output": "nvt",
+            "output_dir": "nvt_equil",
+        },
+        "npt": {
+            "inputs": {
+                "mdp": {"directory": "mdp", "filename": "npt.json"},
+            },
+            "expected_output": "npt",
+            "output_dir": "npt_equil",
+        },
+        "sim": {
+            "inputs": {
+                "mdp": {"directory": "mdp", "filename": "sim.json"},
+            },
+            "expected_output": "sim",
+            "output_dir": "sim",
+        },
+    },
 ) as setup_single:
     setup_single.doc = """Set up and equilibrate a system."""
 
     setup_params = {
-        "inputs": {
-            "pdb": {"directory": "ala_pentapeptide", "filename": "ala_pentapeptide.pdb"}
-        },
-        "output_dir": "{{ params.output_dir }}/prep",
-        "box_size": 3.2,
-        "force_field": "amber99sb-ildn",
-        "water_model": "tip3p",
-        "ion_concentration": 0.15,
-        "expected_output": "system_prepared.gro",
+        "inputs": "{{ params.setup.inputs }}",
+        "output_dir": "{{ params.output_dir }}/{{ params.setup.output_dir }}",
+        "box_size": "{{ params.setup.box_size }}",
+        "force_field": "{{ params.setup.force_field }}",
+        "water_model": "{{ params.setup.water_model }}",
+        "ion_concentration": "{{ params.setup.ion_concentration }}",
+        "expected_output": "{{ params.setup.expected_output }}.gro",
     }
     setup = run_if_needed.override(group_id="prepare_system")(
         dag_id="prepare_system", dag_params=setup_params
@@ -45,18 +87,18 @@ with DAG(
 
     minimize_params = {
         "inputs": {
-            "mdp": {"directory": "mdp", "filename": "min.json"},
+            "mdp": "{{ params.minimize.inputs.mdp }}",
             "gro": {
-                "directory": "{{ params.output_dir }}/prep",
-                "filename": "system_prepared.gro",
+                "directory": "{{ params.output_dir }}/{{ params.setup.output_dir }}",
+                "filename": "{{ params.setup.expected_output }}.gro",
             },
             "top": {
-                "directory": "{{ params.output_dir }}/prep",
-                "filename": "system_prepared.top",
+                "directory": "{{ params.output_dir }}/{{ params.setup.output_dir }}",
+                "filename": "{{ params.setup.expected_output }}.top",
             },
         },
-        "output_dir": "{{ params.output_dir }}/em",
-        "expected_output": "em.gro",
+        "output_dir": "{{ params.output_dir }}/{{ params.minimize.output_dir }}",
+        "expected_output": "{{ params.minimize.expected_output }}.gro",
     }
     minimize = run_if_needed.override(group_id="minimize")(
         dag_id="simulate_no_cpt",
@@ -66,15 +108,18 @@ with DAG(
 
     nvt_params = {
         "inputs": {
-            "mdp": {"directory": "mdp", "filename": "nvt.json"},
-            "gro": {"directory": "{{ params.output_dir }}/em", "filename": "em.gro"},
+            "mdp": "{{ params.nvt.inputs.mdp }}",
+            "gro": {
+                "directory": "{{ params.output_dir }}/{{ params.minimize.output_dir }}",
+                "filename": "{{ params.minimize.expected_output }}.gro",
+            },
             "top": {
-                "directory": "{{ params.output_dir }}/prep",
-                "filename": "system_prepared.top",
+                "directory": "{{ params.output_dir }}/{{ params.setup.output_dir }}",
+                "filename": "{{ params.setup.expected_output }}.top",
             },
         },
-        "output_dir": "{{ params.output_dir }}/nvt_equil",
-        "expected_output": "nvt.gro",
+        "output_dir": "{{ params.output_dir }}/{{ params.nvt.output_dir }}",
+        "expected_output": "{{ params.nvt.expected_output }}.gro",
     }
     nvt_equil = run_if_needed.override(group_id="nvt_equil")(
         dag_id="simulate_no_cpt", dag_params=nvt_params, dag_display_name="nvt_equil"
@@ -82,22 +127,22 @@ with DAG(
 
     npt_params = {
         "inputs": {
-            "mdp": {"directory": "mdp", "filename": "npt.json"},
+            "mdp": "{{ params.npt.inputs.mdp }}",
             "gro": {
-                "directory": "{{ params.output_dir }}/nvt_equil",
-                "filename": "nvt.gro",
+                "directory": "{{ params.output_dir }}/{{ params.nvt.output_dir }}",
+                "filename": "{{ params.nvt.expected_output }}.gro",
             },
             "top": {
-                "directory": "{{ params.output_dir }}/prep",
-                "filename": "system_prepared.top",
+                "directory": "{{ params.output_dir }}/{{ params.setup.output_dir }}",
+                "filename": "{{ params.setup.expected_output }}.top",
             },
         },
-        "output_dir": "{{ params.output_dir }}/npt_equil",
-        "expected_output": "npt.gro",
+        "output_dir": "{{ params.output_dir }}/{{ params.npt.output_dir }}",
+        "expected_output": "{{ params.npt.expected_output }}.gro",
     }
     npt_equil_has_run = verify_files.override(task_id="npt_equil_has_run")(
-        input_dir="{{ params.output_dir }}/npt_equil",
-        filename="npt.gro",
+        input_dir="{{ params.output_dir }}/{{ params.npt.output_dir }}",
+        filename="{{ params.npt.expected_output }}.gro",
     )
     npt_equil = run_if_false.override(group_id="npt_equil")(
         dag_id="simulate_no_cpt",
@@ -108,26 +153,26 @@ with DAG(
 
     sim_params = {
         "inputs": {
-            "mdp": {"directory": "mdp", "filename": "sim.json"},
+            "mdp": "{{ params.sim.inputs.mdp }}",
             "gro": {
-                "directory": "{{ params.output_dir }}/npt_equil",
-                "filename": "npt.gro",
+                "directory": "{{ params.output_dir }}/{{ params.npt.output_dir }}",
+                "filename": "{{ params.npt.expected_output }}.gro",
             },
             "cpt": {
-                "directory": "{{ params.output_dir }}/npt_equil",
-                "filename": "npt.cpt",
+                "directory": "{{ params.output_dir }}/{{ params.npt.output_dir }}",
+                "filename": "{{ params.npt.expected_output }}.cpt",
             },
             "top": {
-                "directory": "{{ params.output_dir }}/prep",
-                "filename": "system_prepared.top",
+                "directory": "{{ params.output_dir }}/{{ params.setup.output_dir }}",
+                "filename": "{{ params.setup.expected_output }}.top",
             },
         },
-        "output_dir": "{{ params.output_dir }}/sim",
-        "expected_output": "sim.gro",
+        "output_dir": "{{ params.output_dir }}/{{ params.sim.output_dir }}",
+        "expected_output": "{{ params.sim.expected_output }}.gro",
     }
     sim_has_run = verify_files.override(task_id="sim_has_run")(
-        input_dir="{{ params.output_dir }}/sim",
-        filename="sim.gro",
+        input_dir="{{ params.output_dir }}/{{ params.sim.output_dir }}",
+        filename="{{ params.sim.expected_output }}.gro",
     )
     simulate = run_if_false.override(group_id="simulate")(
         dag_id="simulate_cpt",
