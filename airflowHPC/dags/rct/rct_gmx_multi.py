632fb2f5bcbaa4a3831fbc8b8a5c51e6df07252e
ejjordan <ejjordan12@gmail.com>
2025-01-17 19:08:10 +0100

    ResourceRCTOperator DAGs

---
+++ airflowHPC/dags/rct/rct_gmx_multi.py
@@ -0,0 +1,74 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.utils import timezone
+from airflowHPC.dags.tasks import get_file
+from airflowHPC.operators import ResourceRCTOperator
+from airflowHPC.utils.mdp2json import update_write_mdp_json_as_mdp_from_file
+
+
+@task
+def outputs_list(**context):
+    num_sims = int(context["task"].render_template("{{ params.num_sims }}", context))
+    output_dir = context["task"].render_template("{{ params.output_dir }}", context)
+    return [f"{output_dir}/sim_{i}" for i in range(num_sims)]
+
+
+with DAG(
+    "rct_gmx_multi",
+    start_date=timezone.utcnow(),
+    catchup=False,
+    params={
+        "output_dir": "rct_gmx_multi",
+        "num_sims": 4,
+        "inputs": {
+            "mdp": {"directory": "mdp", "filename": "basic_md.json"},
+            "gro": {"directory": "ensemble_md", "filename": "sys.gro"},
+            "top": {"directory": "ensemble_md", "filename": "sys.top"},
+        },
+    },
+) as rct_gmx_multi:
+    input_gro = get_file.override(task_id="get_gro")(
+        input_dir="{{ params.inputs.gro.directory }}",
+        file_name="{{ params.inputs.gro.filename }}",
+    )
+    input_top = get_file.override(task_id="get_top")(
+        input_dir="{{ params.inputs.top.directory }}",
+        file_name="{{ params.inputs.top.filename }}",
+    )
+    input_mdp = get_file.override(task_id="get_mdp")(
+        input_dir="{{ params.inputs.mdp.directory }}",
+        file_name="{{ params.inputs.mdp.filename }}",
+    )
+    mdp_sim = update_write_mdp_json_as_mdp_from_file.override(task_id="mdp_sim_update")(
+        mdp_json_file_path=input_mdp,
+        update_dict={"nsteps": 1000},
+    )
+    grompp_result = ResourceRCTOperator(
+        task_id="grompp",
+        executor_config={
+            "mpi_ranks": 1,
+            "cpus_per_task": 1,
+            "gpus": 0,
+            "gpu_type": None,
+        },
+        gmx_executable="gmx_mpi",
+        gmx_arguments=["grompp"],
+        input_files={"-f": mdp_sim, "-c": input_gro, "-p": input_top},
+        output_files={"-o": "run.tpr"},
+        output_dir="{{ params.output_dir }}",
+    )
+    outputs_dirs = outputs_list.override(task_id="get_output_dirs")()
+    mdrun_result = ResourceRCTOperator.partial(
+        task_id="mdrun",
+        executor_config={
+            "mpi_ranks": 1,
+            "cpus_per_task": 2,
+            "gpus": 0,
+            "gpu_type": None,
+        },
+        gmx_executable="gmx_mpi",
+        gmx_arguments=["mdrun"],
+        input_files={"-s": "{{ ti.xcom_pull(task_ids='grompp')['-o'] }}"},
+        output_files={"-c": "result.gro", "-x": "result.xtc"},
+    ).expand(output_dir=outputs_dirs)
+    grompp_result >> mdrun_result
